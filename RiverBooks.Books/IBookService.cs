ef4281eed625965459b6dc568fae69e596c5b08a
sebastiencozzi <sebastien.cozzi@gmail.com>
2026-07-07 17:49:19 +0200

    First commit

---
+++ RiverBooks.Books/IBookService.cs
@@ -0,0 +1,22 @@
+﻿namespace RiverBooks.Books
+{
+    internal interface IBookService
+    {
+        IEnumerable<BookDto> ListBooks();
+    }
+
+    internal class BookService : IBookService
+    {
+        public IEnumerable<BookDto> ListBooks()
+        {
+            return [
+                new BookDto(Guid.NewGuid(), "Abb", "Gérard"),
+                new BookDto(Guid.NewGuid(), "bbb", "Bouchard"),
+                new BookDto(Guid.NewGuid(), "Ccc", "Charlie"),
+                new BookDto(Guid.NewGuid(), "Sarteck", "Bravo"),
+                ];
+
+        }
+    }
+    public record BookDto(Guid id, string Title, string Author);
+}
