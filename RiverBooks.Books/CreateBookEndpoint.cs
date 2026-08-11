fe247eb38312cdefebe68c8393e5ad0f756e68ea
sebastiencozzi <sebastien.cozzi@gmail.com>
2026-08-11 08:15:24 +0200

    create book

---
+++ RiverBooks.Books/CreateBookEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using FastEndpoints;
+namespace RiverBooks.Books;
+
+internal class CreateBookEndpoint(IBookService bookService) : Endpoint<CreateBookRequest, BookDto>
+{
+  private readonly IBookService _bookService = bookService;
+
+  public override void Configure()
+  {
+    Post($"/books");
+    AllowAnonymous();
+  }
+
+  public override async Task HandleAsync(CreateBookRequest req, CancellationToken ct)
+  {
+    var newBookDto = new BookDto(
+      req.Id == Guid.Empty ? Guid.NewGuid() : req.Id,
+      req.Title,
+      req.Author,
+      req.Price,
+      req.Description
+      );
+
+    //todo : see how to handle already existing id case
+
+    await _bookService.CreateBookAsync(newBookDto);
+
+    await Send.CreatedAtAsync<GetBookByIdEndpoint>(new { newBookDto.Id },
+      newBookDto);
+  }
+}
---
+++ RiverBooks.Books/CreateBookRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace RiverBooks.Books;
+
+internal record CreateBookRequest(Guid Id, string Title, string Author, decimal Price, string Description);
